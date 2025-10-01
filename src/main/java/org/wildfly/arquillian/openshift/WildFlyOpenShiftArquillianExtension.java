6b2edd2d8da26752f2bfd6bf6a1e0368fecf82b4
Richard Achmatowicz <rachmato@redhat.com>
2025-10-01 09:46:48 -0400

    Use MetaInfServices annotation to register loadable service.

--- src/main/java/org/wildfly/arquillian/openshift/WildFlyOpenShiftArquillianExtension.java
+++ src/main/java/org/wildfly/arquillian/openshift/WildFlyOpenShiftArquillianExtension.java
@@ -1,8 +1,10 @@
 package org.wildfly.arquillian.openshift;
 
+import org.kohsuke.MetaInfServices;
 import org.jboss.arquillian.container.spi.client.container.DeployableContainer;
 import org.jboss.arquillian.core.spi.LoadableExtension;
 
+@MetaInfServices(LoadableExtension.class)
 public class WildFlyOpenShiftArquillianExtension implements LoadableExtension {
 
     public void register(ExtensionBuilder builder) {
